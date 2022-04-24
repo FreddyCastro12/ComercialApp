59ceae1b748c254a57cf0f343245588dabdd542c
Jhon Freddy Castro Madrid <jhonfreddycastrom@gmail.com>
2022-04-23 21:43:55 -0500

    Prueba locales

--- Template/src/app/pages/local/local.component.ts
+++ Template/src/app/pages/local/local.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Local } from '../components/local_component/Model/Local';
+import { ServiceService } from '../components/local_component/Service/service.service';
 
 @Component({
   selector: 'app-local',
   templateUrl: './local.component.html',
-  styleUrls: ['./local.component.scss']
+  styleUrls: ['./local.component.scss'],
+  providers: [ServiceService]
 })
 export class LocalComponent implements OnInit {
 
-  constructor() { }
+  local: Local = new Local;
+  constructor(public localService: ServiceService) { }
 
   ngOnInit(): void {
   }
 
+  addLocal() {
+    console.log(this.local);
+  }
 }
