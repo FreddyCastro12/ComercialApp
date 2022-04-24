16a659da9c46df7180e219cfe001adf8baad3908
Jhon Freddy Castro Madrid <jhonfreddycastrom@gmail.com>
2022-04-23 23:18:57 -0500

    CRUD Completo Locales Front

--- Template/src/app/pages/local/local.component.ts
+++ Template/src/app/pages/local/local.component.ts
@@ -11,12 +11,48 @@ import { ServiceService } from '../components/local_component/Service/service.se
 export class LocalComponent implements OnInit {
 
   local: Local = new Local;
+  locales: Local[];
   constructor(public localService: ServiceService) { }
 
   ngOnInit(): void {
+    this.getlocales();
   }
 
-  addLocal() {
-    console.log(this.local);
+  addLocal() { 
+    if (this.local.id) {
+      this.localService.updateLocal(this.local.id, this.local).subscribe(res => {
+        let respuesta = JSON.parse(JSON.stringify(res));
+        console.log(respuesta);
+        window.location.reload();
+      });
+    } else {
+      this.localService.createLocal(this.local).subscribe(res => {
+        let respuesta = JSON.parse(JSON.stringify(res));
+        console.log(respuesta);
+        window.location.reload();
+      });
+    }
+  }
+
+  getlocales() {
+    this.localService.listLocals().subscribe(res => {
+      let respuesta = JSON.parse(JSON.stringify(res));   
+      this.locales = respuesta as Local[];
+      console.log(this.locales);      
+    })
+  }
+
+  updateLocal(local: Local) {
+    this.local = local;
+  }
+
+  deleteLocal(id: number) {
+    if (confirm('Esta seguro de eliminar este registro')) {
+      this.localService.deleteLocal(id).subscribe(res => {
+        let respuesta = JSON.parse(JSON.stringify(res));
+        console.log(respuesta);
+        window.location.reload();
+      });
+    }
   }
 }
